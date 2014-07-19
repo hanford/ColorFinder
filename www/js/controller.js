c182c235fa6a0fe06e99876d507a69fc783f78fb
Jack Hanford <jackhanford@gmail.com>
2014-07-19 10:56:06 -0400

    clear button, rgb added

--- www/js/controller.js
+++ www/js/controller.js
@@ -2,45 +2,44 @@ angular.module('controller', [])
 
 .controller('ColorController', function($scope) {
 	$scope.colorCode = '#'
-	$scope.colorObj = {};
 	$scope.colorArray = [];
+	$scope.toggleClear = true;
 
 	$scope.lightenColor = function(colorCode) {
 		var color = Color(colorCode);
-		for (var i = 0; i < 5; i++) {
+		for (var i = 0; i < 15; i++) {
 			$scope.lighten(color);
 		}
 	}
 
 	$scope.darkenColor = function(colorCode) {
 		var color = Color(colorCode);
-		for (var i = 0; i < 5; i++) {
+		for (var i = 0; i < 10; i++) {
 			$scope.darken(color);
 		}
 	}
 
 	$scope.lighten = function (color) {
 		var toHex = color.lighten(0.2).hexString();
+		$scope.toggleClear = false;
 		if (toHex != "#FFFFFF"){
-		$scope.colorCode = toHex;
-		var newCol = {'color': toHex};
-		$scope.colorArray.push(newCol)
+			var newCol = {'color': toHex, 'rgb': color.values.rgb};
+			$scope.colorArray.push(newCol)
 		}
-	    // $('<div class="bg">' + lighter.hexString() + '</div>')
-	    // .css("background-color", lighter.hexString())
-	    // .appendTo('.box');
-	  // }
 	}
 
 	$scope.darken = function (color) {
 		var toHex = color.darken(0.2).hexString();
+		$scope.toggleClear = false;
 		if (toHex != "#000000"){
-		var newCol = {'color': toHex};
-		$scope.colorArray.push(newCol)
+			var newCol = {'color': toHex, 'rgb': color.values.rgb};
+			$scope.colorArray.push(newCol)
 		}
-	    // $('<div class="bg">' + darker.hexString() + '</div>')
-	    // .css("background-color", darker.hexString())
-	    // .appendTo('.box');
-	  // }
+	}
+
+	$scope.clearPalette = function() {
+		$scope.colorArray = [];
+		$scope.toggleClear = true;
+		console.log(colorArray)
 	}
 })
